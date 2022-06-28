7b7b6d14dd8ad2a95c3f762172268298469c6aed
tim.jing@derbysoft.net <julianjing@aliyun>
2022-06-28 23:34:30 +0800

    104

--- 104. Maximum Depth of Binary Tree.go
+++ 104. Maximum Depth of Binary Tree.go
@@ -1,3 +1,14 @@
 package main
 
-//TODO
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	right := maxDepth(root.Right)
+	left := maxDepth(root.Left)
+	if right > left {
+		return right + 1
+	} else {
+		return left + 1
+	}
+}
---
+++ 104. Maximum Depth of Binary Tree_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"e1", args{root: NewTree([]int{3, 9, 20, -1, -1, 15, 7})}, 3},
+		{"e2", args{root: NewTree([]int{1, -1, 2})}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
