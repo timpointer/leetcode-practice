3aceb47a3c52fbb280c60c4d3555876bee53c93e
tim.jing@derbysoft.net <julianjing@aliyun>
2022-06-29 23:49:52 +0800

    160

--- 160. Intersection of Two Linked Lists.go
+++ 160. Intersection of Two Linked Lists.go
@@ -1,3 +1,37 @@
 package main
 
-//TODO
+// TODO have better method
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a := headA
+	b := headB
+
+	for a != nil {
+		for b != nil {
+			if a == b {
+				return a
+			}
+			b = b.Next
+		}
+		b = headB
+		a = a.Next
+	}
+	return nil
+}
+
+func getIntersectionNode2(headA, headB *ListNode) *ListNode {
+	kv := map[*ListNode]int{}
+	a := headA
+	b := headB
+	for a != nil {
+		kv[a] = 1
+		a = a.Next
+	}
+	for b != nil {
+		_, ok := kv[b]
+		if ok == true {
+			return b
+		}
+		b = b.Next
+	}
+	return nil
+}
---
+++ 160. Intersection of Two Linked Lists_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getIntersectionNode(t *testing.T) {
+	n1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	n2 := &ListNode{
+		Val:  1,
+		Next: n1,
+	}
+	n3 := &ListNode{
+		Val:  1,
+		Next: n1,
+	}
+	type args struct {
+		headA *ListNode
+		headB *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"e1", args{
+			headA: n2,
+			headB: n3,
+		}, n1},
+		{"e1", args{
+			headA: NewLinkList([]int{1}),
+			headB: NewLinkList([]int{2}),
+		}, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getIntersectionNode(tt.args.headA, tt.args.headB); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getIntersectionNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
